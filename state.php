a8a84456b2a5e52154e3456a91bf2f968401fdce
JeffersonG999 <72932666+JeffersonG999@users.noreply.github.com>
2023-02-21 22:51:58 +0100

    Create state.php

---
+++ state.php
@@ -0,0 +1,10 @@
+https://api.drupal.org/api/drupal/core!lib!Drupal!Core!State!State.php/class/State
+https://www.drupal.org/docs/8/api/state-api/overview
+
+<?php
+
+\Drupal::time()->getRequestTime())
+  
+\Drupal::state()->get('system.cron_last');
+\Drupal::state()->set('system.cron_last', '1516556165');
+\Drupal::state()->delete('system.cron_last');
