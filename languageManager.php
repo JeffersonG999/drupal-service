c62331582da28d23ebf1922655affc45185517d0
JeffersonG999 <72932666+JeffersonG999@users.noreply.github.com>
2023-02-25 23:12:03 +0100

    Update languageManager.php

--- languageManager.php
+++ languageManager.php
@@ -2,24 +2,66 @@ https://api.drupal.org/api/drupal/core%21lib%21Drupal%21Core%21Language%21Langua
 
 <?php
 
-// Return object of current language and attributes
+// Returns the current language for the given type.
 \Drupal::languageManager()->getCurrentLanguage();
 \Drupal::languageManager()->getCurrentLanguage()->getName();
 \Drupal::languageManager()->getCurrentLanguage()->getId();
 \Drupal::languageManager()->getCurrentLanguage()->getDirection();
 \Drupal::languageManager()->getCurrentLanguage()->getWeight();
-\Drupal::languageManager()->getCurrentLanguage()->getLocked();
+//\Drupal::languageManager()->getCurrentLanguage()->getLocked();
 
-// Return object of default language
+// Returns a language object representing the site's default language.
 \Drupal::languageManager()->getDefaultLanguage();
 \Drupal::languageManager()->getDefaultLanguage()->getName();
 \Drupal::languageManager()->getDefaultLanguage()->getId();
 \Drupal::languageManager()->getDefaultLanguage()->getDirection();
 \Drupal::languageManager()->getDefaultLanguage()->getWeight();
-\Drupal::languageManager()->getDefaultLanguage()->getLocked();
+//\Drupal::languageManager()->getDefaultLanguage()->getLocked();
 
-\Drupal::languageManager()->getLanguage('langcode');
-\Drupal::languageManager()->getLanguages();
+// Returns a list of the default locked languages.
+$lockedLanguages = \Drupal::languageManager()->getDefaultLockedLanguages();
+foreach ($lockedLanguages as $id => $lockedLanguage) {
+  $lockedLanguage->getName();
+  $lockedLanguage->getId();
+  $lockedLanguage->getDirection();
+  $lockedLanguage->getWeight();
+  //$lockedLanguage->getLocked();
+}
+
+// Returns information about all defined language types.
+$languageTypes = \Drupal::languageManager()->getDefinedLanguageTypesInfo();
+$languageTypes['language_interface']['name'];
+$languageTypes['language_interface']['description'];
+$languageTypes['language_interface']['locked'];
+$languageTypes['language_content']['name'];
+$languageTypes['language_content']['description'];
+$languageTypes['language_content']['locked'];
+$languageTypes['language_url'];
+
+// Returns a language object from the given language code.
+\Drupal::languageManager()->getLanguage('fr');
+\Drupal::languageManager()->getLanguage('fr')->getName();
+\Drupal::languageManager()->getLanguage('fr')->getId();
+\Drupal::languageManager()->getLanguage('fr')->getDirection();
+\Drupal::languageManager()->getLanguage('fr')->getWeight();
+//\Drupal::languageManager()->getLanguage('fr')->getLocked();
+
+// Produced the printed name for a language for display.
+\Drupal::languageManager()->getLanguageName('fr');
+
+// Returns a list of languages set up on the site.
+$languages = \Drupal::languageManager()->getLanguages();
+foreach ($languages as $language) {
+  $language->getName();
+  $language->getId();
+  $language->getDirection();
+  $language->getWeight();
+  //$anguage->getLocked();
+}
+
+// Returns an array of the available language types.
+\Drupal::languageManager()->getLanguageTypes()
+
 \Drupal::languageManager()->reset();
 \Drupal::languageManager()->isMultilingual();
 \Drupal::languageManager()->getDefaultLanguage()->getId();
