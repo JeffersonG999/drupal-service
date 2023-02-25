36bc4a335d5f468fc6ede26228ae5906ba16329a
JeffersonG999 <72932666+JeffersonG999@users.noreply.github.com>
2023-02-25 21:23:12 +0100

    Update languageManager.php

--- languageManager.php
+++ languageManager.php
@@ -10,6 +10,13 @@ https://api.drupal.org/api/drupal/core%21lib%21Drupal%21Core%21Language%21Langua
 \Drupal::languageManager()->getCurrentLanguage()->getWeight();
 \Drupal::languageManager()->getCurrentLanguage()->getLocked();
 
+// Return object of default language
+\Drupal::languageManager()->getDefaultLanguage();
+\Drupal::languageManager()->getDefaultLanguage()->getName();
+\Drupal::languageManager()->getDefaultLanguage()->getId();
+\Drupal::languageManager()->getDefaultLanguage()->getDirection();
+\Drupal::languageManager()->getDefaultLanguage()->getWeight();
+\Drupal::languageManager()->getDefaultLanguage()->getLocked();
 
 \Drupal::languageManager()->getLanguage('langcode');
 \Drupal::languageManager()->getLanguages();
