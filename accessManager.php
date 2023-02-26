2c50b6501498f8c14beff115e3e8d66eb9709ad3
JeffersonG999 <72932666+JeffersonG999@users.noreply.github.com>
2023-02-26 13:08:30 +0100

    Update accessManager.php

--- accessManager.php
+++ accessManager.php
@@ -1,3 +1,25 @@
 <?php
 
-\Drupal::accessManager();
+use Drupal\Core\Routing\RouteMatch;
+use Drupal\Core\Routing\RouteObjectInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Route;
+
+// Checks a route against applicable access check services.
+\Drupal::accessManager()->checkNamedRoute('web.example', array(), NULL, FALSE);
+// Return 1 if access or 0 
+\Drupal::accessManager()->checkNamedRoute('web.example', array(), NULL, TRUE);
+// Return object AccessResult
+
+// Execute access checks against the incoming request
+$route = new Route('/web/example');
+$request = new Request();
+$request->attributes->set(RouteObjectInterface::ROUTE_OBJECT, $route);
+\Drupal::accessManager()->checkRequest($request, NULL, FALSE);
+
+// Checks a route against applicable access check services.
+$route = new Route('/web/example');
+$request = new Request();
+$request->attributes->set(RouteObjectInterface::ROUTE_OBJECT, $route);
+$route_match = RouteMatch::createFromRequest($request);
+\Drupal::accessManager()->check($route_match, NULL, NULL, FALSE);
