33ee2551061caffbc934a9ba3bb96f9d2c5a7e9e
JeffersonG999 <72932666+JeffersonG999@users.noreply.github.com>
2023-03-03 12:55:41 +0100

    Update currentUser.php

--- currentUser.php
+++ currentUser.php
@@ -1,5 +1,14 @@
 <?php
 
+use Drupal\user\Entity\User;
+
+// Return User information
+$user = \Drupal::currentUser();
+$user->id();
+$user->getRoles();
+$user->getTimezone();
+
+// Return More User information
 $user = \Drupal::currentUser()->getAccount();
 $user->id();
 $user->getRoles();
@@ -14,3 +23,10 @@ $user->login;
 $user->init;
 $user->default_langcode;
 
+// Avoid to use
+\Drupal::currentUser()->setAccount();
+// Avoid to use
+\Drupal::currentUser()->setInitialAccountId();
+
+// Return Full User information
+User::load(\Drupal::currentUser()->id());
