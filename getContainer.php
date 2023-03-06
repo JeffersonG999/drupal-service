874efd868c5dadd219d15bd0e7537c583ab2c319
JeffersonG999 <72932666+JeffersonG999@users.noreply.github.com>
2023-03-06 22:27:28 +0100

    Create getContainer.php

---
+++ getContainer.php
@@ -0,0 +1,8 @@
+<?php
+
+\Drupal::getContainer()->hasParameter('site.path');
+\Drupal::getContainer()->getParameter('site.path');
+\Drupal::getContainer()->hasParameter('cache_bins');
+\Drupal::getContainer()->getParameter('cache_bins');
+\Drupal::getContainer()->hasParameter('app.root');
+\Drupal::getContainer()->getParameter('app.root');
