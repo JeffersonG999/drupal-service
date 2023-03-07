b7a657c7138c29d58d95ef8a23670bf796cebba3
JeffersonG999 <72932666+JeffersonG999@users.noreply.github.com>
2023-03-07 23:23:53 +0100

    Update httpClient.php

--- httpClient.php
+++ httpClient.php
@@ -1,4 +1,6 @@
 https://www.drupal.org/docs/contributed-modules/http-client-manager
+https://www.census.gov/data/developers/guidance/api-user-guide/example-api-queries.html
+https://swapi.dev/api/people
 
 <?php
 $request = \Drupal::httpClient()->request('GET', 'https://api.census.gov/data/2019/acs/acs1?get=NAME,B02015_009E,B02015_009M', [
@@ -6,6 +8,9 @@ $request = \Drupal::httpClient()->request('GET', 'https://api.census.gov/data/20
   'sort' => 'ASC',
 ]);
 $request = \Drupal::httpClient()->request('GET', 'https://api.census.gov/data/timeseries/intltrade/exports/enduse?get=DISTRICT,DIST_NAME,ALL_VAL_MO,ALL_VAL_YR&YEAR=2016&MONTH=06');
+$request = \Drupal::httpClient()->get('https://api.census.gov/data/timeseries/intltrade/exports/enduse?get=DISTRICT,DIST_NAME,ALL_VAL_MO,ALL_VAL_YR&YEAR=2016&MONTH=06');
 $status_code = $request->getStatusCode();
 $headers = $request->getHeaders();
+//$body = $request->getBody();
+$body_json = json_decode($request->getBody(), TRUE);
 $posts = $request->getBody()->getContents();
