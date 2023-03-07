1be16b4d29e7d345fb8ac160e3c60e2befe7cb29
JeffersonG999 <72932666+JeffersonG999@users.noreply.github.com>
2023-03-07 22:47:37 +0100

    Create httpClient.php

---
+++ httpClient.php
@@ -0,0 +1,11 @@
+<?php
+
+$request = \Drupal::httpClient()->request('GET', 'https://api.census.gov/data/2019/acs/acs1?get=NAME,B02015_009E,B02015_009M', [
+  'limit' => 10,
+  'sort' => 'ASC',
+]);
+$request = \Drupal::httpClient()->request('GET', 'https://api.census.gov/data/timeseries/intltrade/exports/enduse?get=DISTRICT,DIST_NAME,ALL_VAL_MO,ALL_VAL_YR&YEAR=2016&MONTH=06');
+if ($request->getStatusCode() != 200) {
+  return $build;
+}
+$posts = $request->getBody()->getContents();
