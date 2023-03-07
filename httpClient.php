1b89f861ff6f0c46ddce7006427aef6cb8494f99
JeffersonG999 <72932666+JeffersonG999@users.noreply.github.com>
2023-03-07 23:03:43 +0100

    Update httpClient.php

--- httpClient.php
+++ httpClient.php
@@ -1,13 +1,11 @@
 https://www.drupal.org/docs/contributed-modules/http-client-manager
 
 <?php
-
 $request = \Drupal::httpClient()->request('GET', 'https://api.census.gov/data/2019/acs/acs1?get=NAME,B02015_009E,B02015_009M', [
   'limit' => 10,
   'sort' => 'ASC',
 ]);
 $request = \Drupal::httpClient()->request('GET', 'https://api.census.gov/data/timeseries/intltrade/exports/enduse?get=DISTRICT,DIST_NAME,ALL_VAL_MO,ALL_VAL_YR&YEAR=2016&MONTH=06');
-if ($request->getStatusCode() != 200) {
-  return $build;
-}
+$status_code = $request->getStatusCode();
+$headers = $request->getHeaders();
 $posts = $request->getBody()->getContents();
