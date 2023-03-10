79662f0b66f93618b7cad72b2b16c5be93bfd187
JeffersonG999 <72932666+JeffersonG999@users.noreply.github.com>
2023-03-10 06:43:51 +0100

    Create logger.php

---
+++ logger.php
@@ -0,0 +1,10 @@
+<?php
+
+\Drupal::logger('jeff')->alert('Alert');
+\Drupal::logger('jeff')->critical('Critical');
+\Drupal::logger('jeff')->debug('Debug');
+\Drupal::logger('jeff')->emergency('Emergency');
+\Drupal::logger('jeff')->error('Error');
+\Drupal::logger('jeff')->info('Info');
+\Drupal::logger('jeff')->notice('Notice');
+\Drupal::logger('jeff')->warning('Warning');
